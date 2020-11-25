a47f690861d37aabeba0fb8feea57922a4ff6d96
Juliana Bernardes Pereira <julianabp96@gmail.com>
2020-11-25 10:46:22 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
